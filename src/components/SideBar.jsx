27e5911c5dbca2faeba5948cac9b721de0da3055
Luke Hopkins <luke.hopkins@outlook.com>
2024-09-30 18:20:20 -0400

    Initial commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,26 @@
+export default function SideBar(props) {
+    const {handleToggleModal, data} = props
+
+
+    return(
+        <div className="sideBar">
+             <div onClick={handleToggleModal} 
+             className="bgOverlay"></div>
+             <div className="sideBarContents">
+             <h2>{data?.title}</h2>
+                <div className="descContainer">
+                    <p className="descTitle">{data?.date}:</p>
+                    <p>{data?.explanation}</p>
+                </div>
+                <button onClick={handleToggleModal}>
+                    <i className="fa-solid fa-circle-right"></i>
+                </button>
+            
+            </div>
+        </div>
+
+    )
+
+
+
+}
