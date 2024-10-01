b67dca03422c415261f7658e4b1f903245ef4e80
Luke Hopkins <luke.hopkins@outlook.com>
2024-10-01 14:40:20 -0400

    Updated the Head file to allow for videos to be played as well, and formatted the CSS for videos.

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -4,11 +4,19 @@ export default function Head(props) {
 
     return(
         <div className="imageContainer">
-                
+                {data.media_type === "image" ?(
                 <img src={data.hdurl} 
                 alt={data.title || 'bg-img'}
-                className="bgImage" />
+                className="bgImage" />) : (
+                <iframe title={data.title}
+                src={data.url}
+                gesture="media"
+                allow="encypted-media"
+                allowFullScreen
+                className="bgVideo" />
 
+                )
+                }
         </div>
    
         
